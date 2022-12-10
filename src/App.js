d6be0ebe00e9ba6a3e57a3548506558d5240574e
Erwin Jongenelen <erwinjongenelen@gmail.com>
2022-12-10 15:51:07 +0100

    Updated name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Erwin Jongenelen</p>
+        <p>Erwin Jongenelen!</p>
       </header>
     </div>
   );
