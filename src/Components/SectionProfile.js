c9d29751ca0be552790745b75f10395650b36098
Erwin Jongenelen <erwinjongenelen@gmail.com>
2022-12-18 16:22:46 +0100

    Removed README file and updated profile texts

--- src/Components/SectionProfile.js
+++ src/Components/SectionProfile.js
@@ -92,18 +92,18 @@ function SectionProfile() {
 		<div ref={profile} id="profile" className="relative overflow-hidden min-h-screen bg-secondary">
 			<div className="container px-4 mx-auto pt-[90px] pb-[150px] lg:pt-[150px] lg:pb-[235px]">
                 <div className="grid gap-4 xl:gap-7 grid-cols-12">
-					<div className="col-span-12 xl:col-span-5">
+					<div className="col-span-12 xl:col-span-6">
 						<blockquote className="profile-content-area pl-4 border-l-[3px] border-solid border-quaternary text-xl md:text-2xl leading-normal mb-8 lg:mb-12">
 							Mijn naam is <strong>Erwin Jongenelen</strong>. <br/>
-							Ik ben een passionele webdeveloper met een goede kennis van frontend en backend.
+							Ik ben een gepassioneerde webdeveloper met een goede kennis van frontend en backend.
 						</blockquote>
 					</div>
 				</div>
 				<div className="grid gap-4 xl:gap-7 grid-cols-12">
 					<div className="col-span-12 xl:col-span-7">
 						<div className="profile-content-area text-md leading-relaxed">
-							<p className="mb-8">Mijn vrienden en collega's zouden me omschrijven als een analytische, perfectionistische en een pragmatische probleemoplosser met een enorme drive om te blijven leren en kennis te verbreden. In mijn vrije tijd leer ik graag bij over onderwerpen die ik interessant vind, houd ik van games, speel ik (bas)gitaar en bezoek ik maar al te graag een gaaf rock concert.</p>
-							<p className="mb-8">Met het maken van custom oplossingen, contact hebben met klanten en collega's, en het besteden van mijn vrije tijd aan het verdiepen in verschillende webtechnieken houd ik mezelf up-to-date als webdeveloper. Ik word immens gelukkig van het maken van uitdagende en ambitieuze projecten in een leuk en informeel team.</p>
+							<p className="mb-8">Mijn vrienden en collega's zouden mij omschrijven als een analytische, perfectionistische en pragmatische probleemoplosser met een enorme drive om te blijven leren en mijn kennis te verbreden. In mijn vrije tijd leer ik graag meer over onderwerpen die ik interessant vind. Zo heb ik bijvoorbeeld geleerd om webapplicaties te ontwikkelen met React en Tailwind, en website elementen te animeren met GSAP. Daarnaast houd ik van gamen op consoles, speel ik (bas)gitaar en bezoek ik maar al te graag een gaaf concert.</p>
+							<p className="mb-8">Ik word immens gelukkig van het maken van uitdagende projecten in een leuk en informeel team. Met het maken van custom oplossingen, het verdiepen in diverse webtechnieken, en het contact hebben met klanten en collega's houd ik mezelf up-to-date als webdeveloper en werknemer.</p>
 							<p className="mb-0">Door de jaren heen ben ik werkzaam geweest bij verschillende werkgevers waarbij ik ervaring heb opgedaan in het bedrijfsleven en het ontwikkelen voor het web. </p>
 						</div>
 					</div>
